21571180c81226ff0f31e0704271f1db11516ad8
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-04-11 17:13:43 +0900

    fix: devise設定修正

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -3,6 +3,7 @@
 DeviseTokenAuth.setup do |config|
   config.change_headers_on_each_request = false
   config.token_lifespan = 2.weeks
+  config.default_confirm_success_url = "confirmed"
   config.headers_names = {:'access-token' => 'access-token',
                         :'client' => 'client',
                         :'expiry' => 'expiry',
